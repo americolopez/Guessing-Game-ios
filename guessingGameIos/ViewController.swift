a077a11d535e107fdc0a005e2c04673b9995166c
Americo Lopez <americolopez@Interapts-MacBook-Pro-83.local>
2018-03-15 13:44:54 -0400

    program is complete

--- guessingGameIos/ViewController.swift
+++ guessingGameIos/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var userGuessField: UITextField!
     @IBOutlet weak var userFeedbackLabel: UILabel!
     @IBOutlet weak var rulesLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     var userGuess :Int?
     var randomNumber = Int(arc4random_uniform(100))
-    var numberOfGuesses = 0
+    var tries: Int = 1
     var rulesLabelText = """
 ⭐️⭐️⭐️⭐️⭐️RULES⭐️⭐️⭐️⭐️⭐️
 rule 1. Guess a number 1-100
@@ -29,29 +31,46 @@ rule 3. You will be given hints
         self.view.backgroundColor = #colorLiteral(red: 0, green: 0.888774693, blue: 1, alpha: 1)
         rulesLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         rulesLabel.text = rulesLabelText
-    
+        answerLabel.text = "\(randomNumber)"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-
-    @IBAction func guessButtonPressed(_ sender: Any) {
-        userGuess = Int(userGuessField.text!)
-        if userGuess! > randomNumber {
-            userFeedbackLabel.text = "your guess was too high try again \(numberOfGuesses+1)/5"
-}else if userGuess! < randomNumber{
-            userFeedbackLabel.text = "your guess was too low try again, you have used this many guess\(numberOfGuesses+1)/5"
-        }else if userGuess == nil {
-            userFeedbackLabel.text = "pls type in a number"
-        }else if userGuess! > 100 || userGuess! < 0 {
-userFeedbackLabel.text = "pls type in a number 0-100"
-        }else{
-           userFeedbackLabel.text = "you got it right, it took this many tries to get it right \(numberOfGuesses)/5"
-        
+    func startOver() {
+        randomNumber = Int(arc4random_uniform(100))
+        tries = tries - 5
+        userFeedbackLabel.text = """
+        you ran out of tries,your game has restrated, you can guess again
+"""
     }
     
+    func game() {
+        if userGuess == nil {
+            userFeedbackLabel.text = "pls type in a number"
+        }else if userGuess! > 100 || userGuess! < 0 {
+            userFeedbackLabel.text = "pls type in a number 0-100"
+        }else if userGuess! < randomNumber{
+            userFeedbackLabel.text = "your guess was too low try again, number of tries used \(tries)"
+            tries = 1 + tries
+        }else if userGuess! > randomNumber {
+            userFeedbackLabel.text = "your guess was too high try again, number of tries used \(tries) "
+            tries = 1 + tries
+        }else{
+            userFeedbackLabel.text = "you got it right, it took this many tries to get it right \(tries) "
+            startOver()
+        }
+    }
+    
+    @IBAction func guessButtonPressed(_ sender: Any) {
+        userGuess = Int(userGuessField.text!)
+        
+        if tries > 5{
+            startOver()
+        }else{
+            game()
+        }
+        
 }
-
 }
